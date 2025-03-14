9a3212dac400053b9330aa91194cd04ad606f4c4
Felipe <felipefabreu@gmail.com>
2025-03-14 14:38:21 -0300

    adjusting for build

--- components/custom-components/task-modal/delete-task-modal.tsx
+++ components/custom-components/task-modal/delete-task-modal.tsx
@@ -10,7 +10,7 @@ interface DeleteTaskModalProps {
   title: string
 }
 
-export default function DeleteTaskModal({
+export function DeleteTaskModal({
   children,
   title,
 }: DeleteTaskModalProps) {
